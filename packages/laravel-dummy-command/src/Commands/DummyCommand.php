3d66bd4cbb31031f055eb1ae29e25bde8742f49d
geangontijo <geangontijo07@gmail.com>
2024-10-30 14:56:22 -0300

    criando comando na v0.0.4

---
+++ packages/laravel-dummy-command/src/Commands/DummyCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GeanGontijo\LaravelDummy\Commands;
+
+use Illuminate\Console\Command;
+
+class DummyCommand extends Command
+{
+    protected $signature = 'dummy:command';
+
+    protected $description = 'This is a dummy command';
+
+    public function handle(): void
+    {
+        $this->info('Tecnologia avançada');
+    }
+
+}
--- packages/laravel-dummy-command/src/DummyCommandServiceProvider.php
+++ packages/laravel-dummy-command/src/DummyCommandServiceProvider.php
@@ -2,8 +2,17 @@
 
 namespace GeanGontijo\LaravelDummy;
 
+use GeanGontijo\LaravelDummy\Commands\DummyCommand;
 use Illuminate\Support\ServiceProvider;
 
 class DummyCommandServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                DummyCommand::class,
+            ]);
+        }
+    }
 }
