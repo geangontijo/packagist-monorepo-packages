dcbcf24341e7295320b3977c6e20d4ef3fe95009
geangontijo <gean.developer.07@gmail.com>
2024-11-06 13:48:08 -0300

    Atualizando mensagem de log para Tecnologia avançada v16

--- packages/laravel-dummy/src/DummyServiceProvider.php
+++ packages/laravel-dummy/src/DummyServiceProvider.php
@@ -9,6 +9,6 @@ class DummyServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Log::emergency('BOOT - Tecnologia avançada v12');
+        Log::emergency('BOOT - Tecnologia avançada v16');
     }
 }
