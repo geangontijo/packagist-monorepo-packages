50f2f795ee104a3689fa149f26c3b281952cad99
Gean Gontijo <gean.developer.07@gmail.com>
2024-11-06 13:04:04 -0300

    Atualizando mensagem de log para Tecnologia avançada v22

--- packages/laravel-dummy/src/DummyServiceProvider.php
+++ packages/laravel-dummy/src/DummyServiceProvider.php
@@ -9,6 +9,6 @@ class DummyServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Log::emergency('BOOT - Tecnologia avançada v18');
+        Log::emergency('BOOT - Tecnologia avançada v22');
     }
 }
